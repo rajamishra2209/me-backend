3cde935e305abecdbb291677c8a875a8743591b3
rajamishra2209 <rajamishra220900@gmail.com>
2024-04-01 18:13:36 +0530

    utils intial code

--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,4 @@
-import mongoose, { connections } from "mongoose";
+import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is listening on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection gety failed", err);
+  });
 
 // import express from "express";
 
