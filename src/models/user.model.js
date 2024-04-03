f6f84645c4fe0cc7875af748a56841f39de1701d
rajamishra2209 <rajamishra220900@gmail.com>
2024-04-03 18:35:22 +0530

    updated files and made user controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -77,7 +77,7 @@ userSchema.methods.generateAccessToken = function(){
 userSchema.methods.generateRefreshToken = function(){
     jwt.sign(
      {
-         _id: this._id,
+         _id: this._id, 
      },
      process.env.REFRESH_TOKEN_SECRET,
      {
